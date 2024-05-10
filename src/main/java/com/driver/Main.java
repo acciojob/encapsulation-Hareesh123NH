7f471def67e839afd4e65160d4762cbcc24e2622
Hareesh123NH <nagasarihareesh2005@gmail.com>
2024-05-10 16:41:36 +0530

    update

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    RWOnly obj=new RWOnly();
+    //System.out.println(obj.name);
+   /*Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
+        The field RWOnly.name is not visible
+
+        at com.driver.Main.main(Main.java:6)*/
+    obj.setter("Hareesh");
+    System.out.println(obj.getter());
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
+import javax.sound.midi.Sequencer;
+
 public class RWOnly {
-    
+    private String name;
+    public void setter(String val){
+        this.name=val;
+    }
+    public String getter(){
+        return name;
+    }
 }
